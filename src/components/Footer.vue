d54d5bb3cc49c47e9123cac11579e3709e51b45d
SieChan <sie.chan@outlook.com>
2020-12-12 13:51:49 -0700

    debugging conducted

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="footer-container">
+        <h4>//GYM NAME HERE//</h4>
+        <h4>Copyright</h4>
+        <h4>By Sie Chan</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "page-footer",
+    }
+</script>
+
+<style scoped>
+#footer-container {
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    grid-template-columns: 1fr 1fr 1fr;
+    width: 100%;
+    height: 10vh;
+    border-top: 2px solid #426A5A;
+}
+h4 {
+    
+    margin-top: 9vh;
+    margin-bottom: 5vh;
+    font-size: 10px;
+}
+</style>
