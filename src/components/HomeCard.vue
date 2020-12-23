b76d2fdf842217bb326fc1dbe3762a54e12270e9
SieChan <sie.chan@outlook.com>
2020-12-22 20:49:23 -0700

    edited design

--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -4,7 +4,7 @@
     >
       <v-carousel
         cycle
-        height="400"
+        min-height="400"
         hide-delimiter-background
         show-arrows-on-hover
        
@@ -28,16 +28,16 @@ export default {
     return {
       items: [
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          src: "https://images.pexels.com/photos/841130/pexels-photo-841130.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         },
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+          src: "https://www.crossfit.com/wp-content/uploads/2020/10/19092755/2018072814475780_ND_ND5_6593-1-copy-1024x576.jpg",
         },
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+          src: "https://www.wellandgood.com/wp-content/uploads/2018/11/Stocksy-crossfit-friends-BONNINSTUDIO.jpg",
         },
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+          src: "https://images.glaciermedia.ca/polopoly_fs/1.24151083.1591942357!/fileImage/httpImage/image.jpg_gen/derivatives/landscape_804/b1-06122020-crossfit-jpg.jpg",
         },
       ],
     };
